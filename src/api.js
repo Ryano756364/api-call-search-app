184d5174da9a2f676d70bd90356c8268f3bc55a2
rodell <ryanodell2668@gmail.com>
2023-02-06 20:34:18 -0500

    Proper setup of data fetching

--- src/api.js
+++ src/api.js
@@ -1,20 +1,15 @@
-// Represent all the code related to API
 import axios from 'axios';
 
-// async and await -> wait for the reponse back before JS runs next line of code
-const searchImages = async () => {
+const searchImages = async (searchTerm) => {
   const response = await axios.get('https://api.unsplash.com/search/photos', {
-    //This is what identifies us with who we are requesting from
     headers: {
       Authorization: 'Client-ID Kn_PxSGKJ8Ft-uz-OKImHZSPYP7tGguVcW-kth8PDzI'
     },
     params: {
-      //The image we are searching for - aka what get's typed (hardcoding for now to test)
-      query: 'dogs'
+      query: searchTerm,
     }
   });
-
-  console.log(response); //Make sure we are getting back data from API
+  console.log(response.data.results); //returns array of images coming back from api
   return response;
 };
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
 import App from './App';
-import searchImages from './api';
-
-searchImages(); //This is just for testing
 
 const el = document.getElementById('root');
 const root = ReactDom.createRoot(el);
